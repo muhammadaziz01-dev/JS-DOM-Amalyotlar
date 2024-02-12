8c9347a4d5f37531c55b0eda60e10d88bbd8c785
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-02-12 18:04:40 +0300

    misolllar

--- tasc-1/script.js
+++ tasc-1/script.js
@@ -1,11 +1,32 @@
-const btn = document.querySelector("#btn");
-const inpt = document.querySelector("#input");
-const card = document.querySelector(".card");
+// const btn = document.querySelector("#btn");
+// const inpt = document.querySelector("#input");
+// const card = document.querySelector(".card");
 
-let arr = [];
-function btnC () {
-    let pi = document.createElement("p");
-    pi.textContent = inpt.value;
-    card.appendChild(pi);
-    inpt.value=""
-};
+// let arr = [];
+// function btnC () {
+//     let pi = document.createElement("p");
+//     pi.textContent = inpt.value;
+//     card.appendChild(pi);
+//     inpt.value=""
+// };
+
+
+let str = "ma";
+
+let arr = [
+    {
+        name: "olma",
+        narh: 1200
+    },
+    {
+        name: "mandalin",
+        narh: 1200
+    },
+    {
+        name: "limon",
+        narh: 1200
+    },
+];
+
+let nyuwArre = arr.filter((el) => el.name.includes(str));
+console.log(nyuwArre);
